f76c10988d1a9432bb9fea52f6c6ad9164807504
sud <sud@sud.(none)>
2020-11-04 14:31:28 -0200

    Tela de cadastro de estado finalizado.

---
+++ src/main/resources/static/Main.js
@@ -0,0 +1,165 @@
+let validaTemContudoDivMenus = "";
+let htmlButtonsSubmenu = "";
+
+$(function () {
+    constructionMenu();
+});
+
+function constructionMenu() {
+    
+    let listMenus = structureMenu();
+    let htmlButtonsCadastro = "";
+    
+    $(".li-pages").on("click", function (target) {
+        htmlButtonsCadastro = "";
+        htmlButtonsSubmenu = "";
+        if ($("#pages").find("div").length <= 1 || validaTemContudoDivMenus !== target.handleObj.selector) {
+            $(".loading").addClass("show");
+            $("#pages").find("div").empty();
+            clearCanvasMyChart();
+            
+            validaTemContudoDivMenus = target.handleObj.selector;
+            
+            $.each(listMenus, function (index) {
+                let menus = listMenus[index];
+                if (menus.length) {
+                    htmlButtonsCadastro += "<div style='margin-top: 40px;' class='container h-100'>";
+                    htmlButtonsCadastro += "<div class='row'>";
+
+                    for (let i = 0; i < menus.length; i++) {
+
+                        htmlButtonsCadastro += "<div style='margin: 0 0 1em 0;' class='col-sm-3'>";
+                            htmlButtonsCadastro += "<div class='card'>";
+                                htmlButtonsCadastro += "<div class='card-body'>";
+                                    htmlButtonsCadastro += "<h5 class='card-title'>" + menus[i].title + "</h5>";
+                                    htmlButtonsCadastro += "<p class='card-text'><blockquote class='blockquote mb-0 blockquote-footer'>" + menus[i].type + "</blockquote></p>";
+                                    
+                                    if (menus[i].submenu && menus[i].submenu.length > 0) {
+                                        
+                                        
+                                        let submenu = menus[i].submenu;
+                                        
+                                        htmlButtonsSubmenu += "<div style='margin-top: 40px;' class='container h-100'>";
+                                        htmlButtonsSubmenu += "<div class='row'>";
+
+                                        for (let sub = 0; sub < submenu.length; sub++) {
+
+                                            htmlButtonsSubmenu += "<div style='margin: 0 0 1em 0;' class='col-sm-3'>";
+                                                htmlButtonsSubmenu += "<div class='card'>";
+                                                    htmlButtonsSubmenu += "<div class='card-body'>";
+                                                        htmlButtonsSubmenu += "<h5 class='card-title'>"+submenu[sub].title+"</h5>";
+                                                        htmlButtonsSubmenu += "<p class='card-text'><blockquote class='blockquote mb-0 blockquote-footer'>" + submenu[sub].type + "</blockquote></p>";
+                                                        htmlButtonsSubmenu += "<button data-url='"+submenu[sub].url+"' onclick='"+submenu[sub].event+"' title='"+submenu[sub].title+"' class='"+submenu[sub].class+"'>"+submenu[sub].name+"</button>";
+                                                    htmlButtonsSubmenu += "</div>";
+                                                htmlButtonsSubmenu += "</div>";
+                                            htmlButtonsSubmenu += "</div>";
+                                        }
+                                        
+                                        htmlButtonsSubmenu += "</div>";
+                                        htmlButtonsSubmenu += "</div>";
+                                        
+                                    }
+                                    
+                                    htmlButtonsCadastro += "<button data-url='"+menus[i].url+"' onclick='"+menus[i].event+"' title='"+menus[i].title+ "'class='"+menus[i].class+"'>"+menus[i].name+"</button>";
+                                htmlButtonsCadastro += "</div>";
+                            htmlButtonsCadastro += "</div>";
+                        htmlButtonsCadastro += "</div>";
+                    }
+                    htmlButtonsCadastro += "</div>";
+                    htmlButtonsCadastro += "</div>";
+                }
+            });
+            $(".loading").removeClass("show");
+            $("#pages").find("div").html(htmlButtonsCadastro);
+        }
+    });
+    
+}
+
+function eventContructionPage(target) {
+
+    $(".loading").addClass("show");
+    $("#pages").find("div").empty();
+    $("#pages").find("div").load(target.dataset.url, function (target) {});
+    $(".loading").removeClass("show");
+    validaTemContudoDivMenus = "";
+    
+}
+
+function eventSubmenu(target) {
+    
+    $(".loading").addClass("show");
+    $("#pages").find("div").empty();
+    $("#pages").find("div").html(htmlButtonsSubmenu);
+    $(".loading").removeClass("show");
+    validaTemContudoDivMenus = "";
+    
+}
+
+function structureMenu() {
+
+    let menus = {
+
+        'cadastros': [
+
+            {
+                'name': 'Estado',
+                'title': 'Cadastro de estado',
+                'type': 'Página',
+                'class':'btn btn-success',
+                'event':'eventContructionPage(this)',
+                'url': 'pages/estado/Estado.html'
+            },
+            {
+                'name': 'Cidade',
+                'title': 'Cadastro de cidade',
+                'type': 'Página',
+                'class':'btn btn-success',
+                'event':'eventContructionPage(this)',
+                'url': 'pages/estado/MenuCadastro.html'
+            },
+            {
+                'name': 'Bairro',
+                'title': 'Cadastro de bairro',
+                'type': 'Página',
+                'class':'btn btn-success',
+                'event':'eventContructionPage(this)',
+                'url': 'pages/estado/MenuCadastro.html'
+            },
+
+            {
+                'name': 'Portaria',
+                'title': 'Cadastro de portaria',
+                'type': 'Menu',
+                'class':'btn btn-info',
+                'event':'eventSubmenu(this)',
+                'url': 'pages/estado/MenuCadastro.html',
+                'submenu': [
+                    {
+                        'name':'Morador',
+                        'title':'Cadastro de morador',
+                        'type':'Página',
+                        'class':'btn btn-success',
+                        'event':'eventContructionPage(this)',
+                        'url':'pages/estado/MenuCadastro.html'
+                    },
+                    {
+                        'name':'Morador',
+                        'title':'Cadastro de morador',
+                        'type':'Página',
+                        'class':'btn btn-success',
+                        'event':'eventContructionPage(this)',
+                        'url':'pages/estado/MenuCadastro.html'
+                    }
+                ]
+            }
+            
+        ]
+    };
+
+    return menus;
+}
+
+function clearCanvasMyChart() {
+    $(".dashboard-myChart").find("div").empty();
+}
---
+++ src/main/resources/static/pages/estado/Salvar.js
@@ -0,0 +1,171 @@
+$(function () {
+
+    $.validator.setDefaults({
+        submitHandler: function () {
+            alert("submitted!");
+        }
+    });
+
+    $("#form-estado").validate({
+        rules: {
+            nome: {
+                required: true,
+                minlength: 2,
+                maxlength: 25
+            }
+        },
+        messages: {
+            nome: {
+                required: "Nome obrigatório",
+                minlength: "Tamanho mínimo para o nome é 2 caracter",
+                maxlength: "Tamanho máximo para no nome é 50 caracter"
+            }
+        },
+        errorElement: "em",
+        errorPlacement: function (error, element) {
+
+            error.addClass("invalid-feedback");
+            if (element.prop("type") === "checkbox") {
+                error.insertAfter(element.next("label"));
+            } else {
+                error.insertAfter(element);
+            }
+        },
+        highlight: function (element, errorClass, validClass) {
+            $(element).addClass("is-invalid").removeClass("is-valid");
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).removeClass("is-invalid");
+            //$(element).addClass("is-valid").removeClass("is-invalid");
+        }
+    });
+    
+
+});
+
+//    $.validator.setDefaults({
+//        submitHandler: function () {
+//            alert("submitted!");
+//        }
+//    });
+//
+//    $(document).ready(function () {
+//        rules: {
+//                firstname: "required",
+//                lastname: "required",
+//                nome: {
+//                    required: true,
+//                    minlength: 2,
+//                    maxlength: 25
+//                },
+//                password: {
+//                    required: true,
+//                    minlength: 5
+//                },
+//                confirm_password: {
+//                    required: true,
+//                    minlength: 5,
+//                    equalTo: "#password"
+//                },
+//                email: {
+//                    required: true,
+//                    email: true
+//                },
+//                agree: "required"
+//            },
+//            messages: {
+//                firstname: "Please enter your firstname",
+//                lastname: "Please enter your lastname",
+//                username: {
+//                    required: "Please enter a username",
+//                    minlength: "Your username must consist of at least 2 characters"
+//                },
+//                password: {
+//                    required: "Please provide a password",
+//                    minlength: "Your password must be at least 5 characters long"
+//                },
+//                confirm_password: {
+//                    required: "Please provide a password",
+//                    minlength: "Your password must be at least 5 characters long",
+//                    equalTo: "Please enter the same password as above"
+//                },
+//                email: "Please enter a valid email address",
+//                agree: "Please accept our policy"
+//            },
+//            errorElement: "em",
+//            errorPlacement: function (error, element) {
+//                // Add the `invalid-feedback` class to the error element
+//                error.addClass("invalid-feedback");
+//
+//                if (element.prop("type") === "checkbox") {
+//                    error.insertAfter(element.next("label"));
+//                } else {
+//                    error.insertAfter(element);
+//                }
+//            },
+//            highlight: function (element, errorClass, validClass) {
+//                $(element).addClass("is-invalid").removeClass("is-valid");
+//            },
+//            unhighlight: function (element, errorClass, validClass) {
+//                $(element).addClass("is-valid").removeClass("is-invalid");
+//            }
+//        });
+//    });
+       
+
+    
+
+
+//    $("#form-estado").validate({
+//        rules: {
+//            nome: {
+//                required: true,
+//                minlength: 6,
+//                maxlength: 25
+//            }
+//        }
+//    });
+//    
+//    jQuery.validator.setDefaults({
+//        errorElement: 'span',
+//        errorPlacement: function (error, element) {
+//            error.addClass('invalid-feedback');
+//            element.closest('.form-group').append(error);
+//        },
+//        highlight: function (element, errorClass, validClass) {
+//            $(element).addClass('is-invalid');
+//        },
+//        unhighlight: function (element, errorClass, validClass) {
+//            $(element).removeClass('is-invalid');
+//        }
+//    });
+
+
+//    $("#form-estado").validate({
+//        rules: {
+//            nome: {
+//                required: true,
+//                minlength: 2,
+//                maxlength: 25
+//            },
+//            messages: {
+//                nome: {
+//                    required: "Nome obrigatório",
+//                    minlength: "Tamanho mínimo para o nome é 2 caracter",
+//                    maxlength: "Tamanho máximo para no nome é 50 caracter"
+//                }
+//            },
+//
+//            errorElement: "span",
+//            errorPlacement: function (error, element) {
+//                error.addClass("invalid-feedback");
+//            },
+//            highlight: function (element, errorClass, validClass) {
+//                $(element).addClass("is-invalid").removeClass("is-valid");
+//            },
+//            unhighlight: function (element, errorClass, validClass) {
+//                $(element).addClass("is-valid").removeClass("is-invalid");
+//            }
+//        }
+//    });
+    
