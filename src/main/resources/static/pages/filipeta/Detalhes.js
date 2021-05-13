d7ecea5196c08fa95da64fa19a995e616ddbbcdb
Hallef <hallefbruno@hotmail.com>
2021-05-13 20:21:20 -0300

    Tela de detalhes

--- src/main/resources/static/pages/filipeta/Detalhes.js
+++ src/main/resources/static/pages/filipeta/Detalhes.js
@@ -5,25 +5,24 @@ $(function () {
     
     $("table").on("click","#btn-detalhe", function () {
         var id = $(this).data("detalhe");
-        
         var url = localStorage.getItem("currentUri")+"/filipetas/buscar/"+id;
         
-        var filipeta = {};
         $.get(url, function(data) {
+            var filipeta = {};
             filipeta = data;
+            $('#modalDetalheFilipeta').modal("show");
+
+            $("#modalDetalheFilipeta").on("shown.bs.modal", function () {
+                var modal = $(this);
+                modal.find("#numero").val(filipeta.numero);
+                modal.find("#portaria").val(filipeta.portaria.nome);
+            });
+
+            $("#modalDetalheFilipeta").on("hidden.bs.modal", function () {
+                $("#modalDetalheFilipeta").modal("dispose");
+            });
         });
         
-        $('#modalDetalheFilipeta').modal("show");
-        
-        $("#modalDetalheFilipeta").on("shown.bs.modal", function () {
-            var modal = $(this);
-            modal.find("#numero").val(filipeta.numero);
-            modal.find("#portaria").val(filipeta.portaria.nome);
-        });
-        
-        $("#modalDetalheFilipeta").on("hidden.bs.modal", function () {
-            $("#modalDetalheFilipeta").modal("dispose");
-        });
     });
     
 
